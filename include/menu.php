7a70ac510bb0f02e7ce0c6cc39c6a3a0c5af74bb
mosaicmuse <93966161+mosaicmuse@users.noreply.github.com>
2022-12-05 21:07:30 -0800

    Add includes

---
+++ include/menu.php
@@ -0,0 +1,33 @@
+<!--START HEADER-->
+    <div class="top-menu">
+      <nav>
+        <div class="logo">
+          <a href="index.html">
+            <img id="logo" src="src/ck-logo.png" alt="Cody Khaos" />
+          </a>
+        </div>
+        <a href="javascript:void(0);" onclick="toggleMenu()"
+          ><img
+            id="hamburger"
+            src="src/hamburger.png"
+            alt="hamburger menu icon"
+        /></a>
+        <ul id="menu">
+          <li class="active"><a href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="work.html">Work</a></li>
+          <li><a href="gallery.html">Gallery</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+    <div id="mobileMenu">
+      <ul>
+        <li class="active"><a href="index.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="work.html">Work</a></li>
+        <li><a href="gallery.html">Gallery</a></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+    </div>
+    <!--END HEADER-->
